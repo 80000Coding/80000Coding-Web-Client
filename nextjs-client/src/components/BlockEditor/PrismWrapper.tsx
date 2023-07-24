1c774c2a0451e58ad0ae469ff0ebede7088565b9
rkskekzzz <wkdlfflxh@naver.com>
2023-07-25 02:08:04 +0900

    ✨ code block language selector 추가

--- nextjs-client/src/components/BlockEditor/PrismWrapper.tsx
+++ nextjs-client/src/components/BlockEditor/PrismWrapper.tsx
@@ -1,6 +1,7 @@
+import cn from 'classnames'
 import Prism from 'prismjs'
 import { highlight, languages } from 'prismjs'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Editor from 'react-simple-code-editor'
 
 import 'prismjs/themes/prism.css'
@@ -11,21 +12,32 @@ type Props = {
 } & React.HTMLAttributes<HTMLTextAreaElement>
 
 const PrismWrapper = ({ code, className, readOnly = false }: Props) => {
-  const [value, setValue] = React.useState(code)
+  const [language, setLanguage] = useState('js')
+  const [value, setValue] = useState(code)
 
   useEffect(() => {
     Prism.highlightAll()
   }, [])
 
   return (
-    <Editor
-      className={className}
-      value={value}
-      onValueChange={setValue}
-      highlight={(code) => highlight(code, languages.js, 'js')}
-      padding={10}
-      readOnly={readOnly}
-    />
+    <div>
+      <Editor
+        className={className}
+        value={value}
+        onValueChange={setValue}
+        highlight={(code) => highlight(code, languages[language], language)}
+        padding={10}
+        readOnly={readOnly}
+      />
+      <select
+        className='absolute left-2 top-2 z-10 bg-transparent text-xs text-gray-500'
+        value={language}
+        onChange={(e) => setLanguage(e.target.value)}
+      >
+        <option value='js'>JavaScript</option>
+        <option value='css'>CSS</option>
+      </select>
+    </div>
   )
 }
 
