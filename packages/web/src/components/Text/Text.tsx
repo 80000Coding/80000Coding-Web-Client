5468c5bea684d4aa30cc7ef1ae48862a2b0b83d4
sehyeon An <sehyeon014@gmail.com>
2023-08-03 23:55:00 +0900

    ✨add Text Component

---
+++ packages/web/src/components/Text/Text.tsx
@@ -0,0 +1,30 @@
+import cn from 'classnames'
+import React from 'react'
+
+type TextType = 'title-1' | 'title-2' | 'title-3' | 'body-1' | 'body-2' | 'body-3' | 'note-1' | 'caption-1' // 여기에 사용할 타입들을 정의합니다.
+
+type Props = {
+  type: TextType
+} & React.HTMLAttributes<HTMLDivElement>
+
+const Text = ({ type, className, children, ...rest }: Props) => {
+  // 타입에 따라서 Tailwind CSS 클래스를 매핑합니다.
+  const typeToClass = {
+    'title-1': 'text-xl font-bold black',
+    'title-2': 'text-lg font-medium text-gray-500',
+    'title-3': 'text-sm font-bold text-gray-700',
+    'body-1': 'text-lg font-bold text-gray-700',
+    'body-2': 'text-lg font-regular text-gray-500',
+    'body-3': 'text-sm font-regular text-gray-700',
+    'note-1': 'text-base font-regular text-gray-700',
+    'caption-1': 'text-xs font-regular text-gray-300',
+  }
+
+  return (
+    <div className={cn(typeToClass[type], className)} {...rest}>
+      {children}
+    </div>
+  )
+}
+
+export default Text
