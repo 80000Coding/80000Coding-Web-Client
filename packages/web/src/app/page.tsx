4757339d1e7585ba51dff256eebd4a5bb37b1f5c
rkskekzzz <wkdlfflxh@naver.com>
2023-08-07 21:19:01 +0900

    ✨ toggle without controller 추가

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
     <main className='flex bg-gray-500 text-gray-500 hover:text-gray-700'>
       Hello World
       <Toggle isChecked={isChecked} onToggleClick={handleClick}></Toggle>
+      <Toggle />
     </main>
   )
 }
