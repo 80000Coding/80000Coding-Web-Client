98e8d98edc891c76165404680123915d7b84d583
rkskekzzz <wkdlfflxh@naver.com>
2023-08-07 23:25:05 +0900

    🐛 css top 속성 버그 수정

--- packages/web/src/components/Toggle/Toggle.const.ts
+++ packages/web/src/components/Toggle/Toggle.const.ts
@@ -1,9 +1,9 @@
 import type { ToggleSliderColor, ToggleSliderSize } from './Toggle.type'
 
 export const toggleSliderSize: ToggleSliderSize = {
-  sm: 'h-[20px] w-[34px] after:h-[16px] after:w-[16px] after:top-[2px] after:left-[2px]',
-  md: 'h-[28px] w-[52px] after:h-[24px] after:w-[24px] after:top-[2px] after:left-[2px]',
-  lg: 'h-[36px] w-[68px] after:h-[32px] after:w-[32px] after:top-[2px] after:left-[2px]',
+  sm: 'h-[20px] w-[36px] after:h-[16px] after:w-[16px] after:top-1/2 after:left-[2px] after:-translate-y-1/2',
+  md: 'h-[28px] w-[52px] after:h-[24px] after:w-[24px] after:top-1/2 after:left-[2px] after:-translate-y-1/2',
+  lg: 'h-[36px] w-[68px] after:h-[32px] after:w-[32px] after:top-1/2 after:left-[2px] after:-translate-y-1/2',
 }
 
 export const toggleSliderColor: ToggleSliderColor = {
