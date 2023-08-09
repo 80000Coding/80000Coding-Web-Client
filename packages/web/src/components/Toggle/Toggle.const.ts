1375ee57207318520615d1fc895553b3ff7c04b0
sehyeon An <sehyeon014@gmail.com>
2023-08-09 23:54:28 +0900

    💄Style: figma디자인 맞춰서 스타일 수정

--- packages/web/src/components/Toggle/Toggle.const.ts
+++ packages/web/src/components/Toggle/Toggle.const.ts
@@ -1,9 +1,9 @@
 import type { ToggleSliderColor, ToggleSliderSize } from './Toggle.type'
 
 export const toggleSliderSize: ToggleSliderSize = {
-  sm: 'h-[20px] w-[36px] after:h-[16px] after:w-[16px] after:top-1/2 after:left-[2px] after:-translate-y-1/2',
-  md: 'h-[28px] w-[52px] after:h-[24px] after:w-[24px] after:top-1/2 after:left-[2px] after:-translate-y-1/2',
-  lg: 'h-[36px] w-[68px] after:h-[32px] after:w-[32px] after:top-1/2 after:left-[2px] after:-translate-y-1/2',
+  sm: 'h-[20px] w-[36px] after:h-[16px] after:w-[16px] after:top-1/2 after:left-[2px] after:-translate-y-1/2 peer-checked:after:translate-x-[16px]',
+  md: 'h-[24px] w-[50px] after:h-[18px] after:w-[18px] after:top-1/2 after:left-[3px] after:-translate-y-1/2 peer-checked:after:translate-x-[26px]',
+  lg: 'h-[28.8px] w-[60px] after:h-[21.6px] after:w-[21.6px] after:top-1/2 after:left-[4px] after:-translate-y-1/2 peer-checked:after:translate-x-[30.4px]',
 }
 
 export const toggleSliderColor: ToggleSliderColor = {
