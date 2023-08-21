d2096c181b7fcb3382369a167554d0bac57c87e6
rkskekzzz <wkdlfflxh@naver.com>
2023-08-21 22:53:52 +0900

    ♻️ storybook builder webpack5 에서 vite로 변경

--- apps/web/.storybook/main.ts
+++ apps/web/.storybook/main.ts
@@ -1,9 +1,29 @@
-import type { StorybookConfig } from '@storybook/nextjs'
-const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+// import type { StorybookConfig } from '@storybook/nextjs'
+// const config: StorybookConfig = {
+//   stories: ['../src/stories/*.mdx', '../src/stories/*.stories.@(js|jsx|mjs|ts|tsx)'],
+//   addons: [
+//     '@storybook/addon-links',
+//     '@storybook/addon-essentials',
+//     '@storybook/addon-onboarding',
+//     '@storybook/addon-interactions',
+//     '@storybook/addon-styling',
+//   ],
+//   framework: {
+//     name: '@storybook/nextjs',
+//     options: {},
+//   },
+//   docs: {
+//     autodocs: 'tag',
+//   },
+// }
+// export default config
+
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-onboarding', '@storybook/addon-interactions'],
   framework: {
-    name: '@storybook/nextjs',
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {
--- apps/web/.storybook/preview.ts
+++ apps/web/.storybook/preview.ts
@@ -1,5 +1,22 @@
+// import '@80000coding/ui/dist/index.css'
+// import '../src/app/globals.css'
+
 import type { Preview } from '@storybook/react'
 
+// const preview: Preview = {
+//   parameters: {
+//     actions: { argTypesRegex: '^on[A-Z].*' },
+//     controls: {
+//       matchers: {
+//         color: /(background|color)$/i,
+//         date: /Date$/,
+//       },
+//     },
+//   },
+// }
+
+// export default preview
+import '@80000coding/ui/dist/index.css'
 import '../src/app/globals.css'
 
 const preview: Preview = {
