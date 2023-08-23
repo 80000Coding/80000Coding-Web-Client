12f25e5b9af4ce03537d7b694b37771066326893
rkskekzzz <wkdlfflxh@naver.com>
2023-08-23 11:18:27 +0900

    💄 workflow trigger 테스트용 색상 변경

--- apps/web/src/stories/Button.stories.ts
+++ apps/web/src/stories/Button.stories.ts
@@ -49,7 +49,7 @@ type Story = StoryObj<typeof meta>
 export const Title: Story = {
   args: {
     variant: 'primary',
-    color: 'blue',
+    color: 'violet',
     disabled: false,
     size: 'M',
     text: '타이틀~',
