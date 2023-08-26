f87cfb06ac88f28ade9459f2a4c3e9f38c8ab285
kunlee <khlee@ebrain.kr>
2023-08-26 17:36:01 +0900

    feat(web): 메인 퀵 패널 메뉴 생성

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,24 +1,16 @@
 'use client'
 
-import { Toggle, Button, LoadingButtonIcon, LoadingPageIcon } from '@80000coding/ui'
-import { useState } from 'react'
-
 import Header from '@/components/common/header'
+import MainQuickPanel from '@/components/main/main-quick-panel'
 
 export default function Home() {
-  const [isChecked, setIsChecked] = useState(false)
-
-  const handleClick = () => {
-    setIsChecked((prevChecked) => !prevChecked)
-  }
-
   return (
     <main>
-      {/* <Header /> */}
-      <LoadingButtonIcon />
-      <LoadingPageIcon />
-      <Toggle isChecked={isChecked} onToggleClick={handleClick}></Toggle>
-      <Toggle />
+      <Header />
+
+      <div>
+        <MainQuickPanel />
+      </div>
     </main>
   )
 }
--- apps/web/src/components/common/header.tsx
+++ apps/web/src/components/common/header.tsx
@@ -9,28 +9,33 @@ import {
   StaticScrapIcon,
 } from '@80000coding/web-icons'
 import { Avatar, Navbar, NavbarBrand, NavbarContent, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from '@nextui-org/react'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 
-const DEFAULT_MENU_ITEMS = [
+export const DEFAULT_MENU_ITEMS = [
   {
     url: '/categories',
     label: '카테고리 모아보기',
+    shortLabel: '카테고리',
     icon: StaticCategoryIcon,
   },
   {
     url: '/projects',
     label: '프로젝트 둘러보기',
+    shortLabel: '프로젝트',
     icon: StaticProjectIcon,
   },
   {
     url: '/hotpost',
     label: '오늘의 게시글 보기',
+    shortLabel: '인기글',
     icon: StaticHotpostIcon,
   },
   {
     url: '/rangkings',
     label: '컨트리뷰터 랭킹 보기',
+    shortLabel: '랭킹',
     icon: StaticRankingIcon,
   },
 ]
@@ -62,7 +67,7 @@ export default function Header() {
     <Navbar classNames={{ wrapper: 'px-5 py-4.5' }} isBlurred={false} height='60px' onMenuOpenChange={setIsMenuOpen}>
       <NavbarContent>
         <NavbarBrand>
-          <img src='/logo/logo.svg' alt='logo' />
+          <Image src='/logo/logo.svg' alt='logo' width={135} height={23} />
         </NavbarBrand>
       </NavbarContent>
       <NavbarContent justify='end'>
@@ -131,7 +136,7 @@ export default function Header() {
         </div>
 
         <Link href='##' className='mb-[73px] w-fit'>
-          <div className='body-3 text-center underline'>'PWA 설치해서 이용하기'</div>
+          <div className='body-3 text-center underline'>PWA 설치해서 이용하기</div>
         </Link>
       </NavbarMenu>
     </Navbar>
