085a69a829d719a3be64640b2c742e3098f18d83
kunlee <khlee@ebrain.kr>
2023-08-26 20:19:20 +0900

    fix(web): leading 적용 후 padding 변경

--- apps/web/src/components/main/main-quick-panel.tsx
+++ apps/web/src/components/main/main-quick-panel.tsx
@@ -5,18 +5,18 @@ import { DEFAULT_MENU_ITEMS } from '../common/header'
 export default function MainQuickPanel() {
   return (
     <div className='flex items-center divide-x divide-gray-300 py-3.5'>
-      <div className='shrink-0 pb-1.5 pl-9 pr-8 pt-0.5'>
+      <div className='shrink-0 pb-2 pl-9 pr-8 pt-1.5'>
         {DEFAULT_MENU_ITEMS.slice(0, 1).map(({ icon: Icon, shortLabel, url }) => (
           <Link href={url} key={shortLabel} className='flex flex-col items-center'>
-            <Icon className='h-9 w-9' />
+            <Icon className='icon-lg' />
             <aside className='caption-1'>{shortLabel}</aside>
           </Link>
         ))}
       </div>
-      <div className='flex w-full items-center justify-between pb-1.5 pl-9 pr-11 pt-0.5'>
+      <div className='flex w-full items-center justify-between pb-2 pl-9 pr-11 pt-1.5'>
         {DEFAULT_MENU_ITEMS.slice(1).map(({ icon: Icon, shortLabel, url }) => (
           <Link href={url} key={shortLabel} className='flex flex-col items-center'>
-            <Icon className='h-9 w-9' />
+            <Icon className='icon-lg' />
             <aside className='caption-1'>{shortLabel}</aside>
           </Link>
         ))}
