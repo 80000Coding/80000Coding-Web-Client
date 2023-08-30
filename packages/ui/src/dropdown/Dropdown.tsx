5e3816cd1256173e9e2166694531450f03295e12
rkskekzzz <wkdlfflxh@naver.com>
2023-08-31 03:16:52 +0900

    ✨ Dropdown 컴포넌트 생성

---
+++ packages/ui/src/dropdown/Dropdown.tsx
@@ -0,0 +1,34 @@
+import type { IconSource } from '@80000coding/web-icons'
+import cn from 'classnames'
+
+type Menu = {
+  title: string
+  titleColor?: string
+  icon: IconSource
+  onClick: () => void
+}
+type DropdownProps = {
+  menus: Menu[]
+} & React.HTMLAttributes<HTMLDivElement>
+
+const Dropdown = ({ menus, className, ...rest }: DropdownProps) => {
+  return (
+    <div className={cn('absolute z-10 rounded-[12px] bg-white shadow-[0px_2px_20px_0px_rgba(198,198,198,0.5)]', className)} {...rest}>
+      {menus.map((menu, index) => (
+        <div
+          key={menu.title}
+          className='relative flex cursor-pointer items-center justify-between gap-[45px] px-[17px] py-[16px]'
+          onClick={menu.onClick}
+        >
+          <span className={cn('body-2', menu.titleColor || 'text-gray-700')}>{menu.title}</span>
+          {menu.icon({})}
+          {menus.length > 1 && index !== menus.length - 1 && (
+            <div className='absolute left-0 top-[100%] h-[1px] w-full -translate-y-1/2 bg-gray-200' />
+          )}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Dropdown
