5738353165841e493694d3b4e31a55e25f33ec4e
rkskekzzz <wkdlfflxh@naver.com>
2023-09-05 22:29:07 +0900

    🐛 bg color white -> gray-200

--- packages/ui/src/notification/NotificationBox.tsx
+++ packages/ui/src/notification/NotificationBox.tsx
@@ -6,7 +6,13 @@ type NotificationBoxProps = {
 
 const NotificationBox = ({ text, children, className, ...rest }: NotificationBoxProps) => {
   return (
-    <div className={cn('caption-3A text-red-warning w-full max-w-md whitespace-pre-wrap rounded-[20px] bg-white px-[20px] py-[15px]', className)} {...rest}>
+    <div
+      className={cn(
+        'caption-3A text-red-warning w-full max-w-md whitespace-pre-wrap rounded-[20px] bg-gray-200 px-[20px] py-[15px]',
+        className,
+      )}
+      {...rest}
+    >
       {text || children}
     </div>
   )
