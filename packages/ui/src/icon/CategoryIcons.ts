e732cecb86c754cd11509800debfb4b81830c99d
rkskekzzz <wkdlfflxh@naver.com>
2023-09-07 01:17:41 +0900

    ✨ category rounded 아이콘 추가 및 categoryIcons prefix제거

--- packages/ui/src/icon/CategoryIcons.ts
+++ packages/ui/src/icon/CategoryIcons.ts
@@ -1,17 +1,18 @@
 import { icons } from '@80000coding/web-icons'
 
 import { categoryIconColor, categoryIconDisplayName } from './CategoryIcons.const'
-import { CategoryIcon, CategoryIconNames } from './CategoryIcons.type'
+import { CategoryIcon, CategoryIconNames, CategoryIconNamesWithoutRounded } from './CategoryIcons.type'
 
 export const categoryIcons = Object.entries(icons)
   .filter(([key]) => key.startsWith('category'))
   .reduce(
     (acc, [key, value]) => {
-      const typesafeKey = key as CategoryIconNames
-      acc[typesafeKey] = {
-        displayName: categoryIconDisplayName[typesafeKey],
-        bgColor: categoryIconColor[typesafeKey].bg,
-        fgColor: categoryIconColor[typesafeKey].fg,
+      const categoryIconName = key.replace('category-', '') as CategoryIconNames
+      const extractedCategoryIconName = categoryIconName.replace('-rounded', '') as CategoryIconNamesWithoutRounded
+      acc[categoryIconName] = {
+        displayName: categoryIconDisplayName[extractedCategoryIconName],
+        bgColor: categoryIconColor[extractedCategoryIconName].bg,
+        fgColor: categoryIconColor[extractedCategoryIconName].fg,
         source: value,
       }
       return acc
