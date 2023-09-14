d6b7c57381fc0e37343515f1a012fab33b11e3a5
rkskekzzz <wkdlfflxh@naver.com>
2023-09-15 02:14:05 +0900

    ✨ feat storybook에서 아이콘 jsx 즉시 복사 기능 추가

--- apps/web/src/stories/icons/Icons.tsx
+++ apps/web/src/stories/icons/Icons.tsx
@@ -1,22 +1,60 @@
-import { icons, StaticAllResultIcon } from '@80000coding/web-icons'
+import { icons, StaticAllIcon } from '@80000coding/web-icons'
 import React from 'react'
 
 type IconsProps = {
   type?: 'category' | 'static' | 'dynamic'
 }
 
+function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function createJsxString(type: IconsProps['type'], key: string) {
+  return (
+    '<' +
+    capitalizeFirstLetter(type || '') +
+    capitalizeFirstLetter(key.split('-')[1]) +
+    'Icon' +
+    (type === 'dynamic' ? ' className="text-green"' : '') +
+    ' />'
+  )
+}
+
 export function Icons({ type }: IconsProps) {
+  const [isCopied, setIsCopied] = React.useState('')
   const typedIcons = Object.entries(icons)
     .filter(([key]) => {
       return key.startsWith(type as string)
     })
     .sort()
+
+  React.useEffect(() => {
+    if (isCopied) {
+      setTimeout(() => {
+        setIsCopied('')
+      }, 1000)
+    }
+  }, [isCopied])
+
   return (
     <div className='grid w-full grid-cols-[repeat(auto-fill,minmax(180px,1fr))]'>
       {typedIcons.map(([key, value]) => (
-        <div key={key} className='flex flex-col items-center gap-4 border border-gray-200 p-4 '>
+        <div
+          key={key}
+          className=' relative flex cursor-pointer flex-col items-center gap-4 border border-gray-200 p-4'
+          onClick={() => {
+            setIsCopied(key)
+            const iconJsx = createJsxString(type, key)
+            window.navigator.clipboard.writeText(iconJsx)
+          }}
+        >
           {value({ className: 'icon-lg ' + (key.startsWith('dynamic') ? 'text-black' : '') })}
           <span className='font-regular text-gray-700'>{key.split('-')[1]}</span>
+          {isCopied === key && (
+            <div className='bg-blue-light/90 absolute left-0 top-0 flex h-full w-full items-center justify-center text-white'>
+              <span>Copied!</span>
+            </div>
+          )}
         </div>
       ))}
     </div>
@@ -36,7 +74,7 @@ export function IconSizes() {
       {iconSizes.concat(iconCustomSizes).map((size) => (
         <div key={size} className='flex flex-row items-center justify-between gap-4 border border-gray-200 p-4'>
           <span className=' font-bold text-gray-700'>{size}</span>
-          <StaticAllResultIcon className={`${size} `} />
+          <StaticAllIcon className={`${size} `} />
         </div>
       ))}
     </div>
