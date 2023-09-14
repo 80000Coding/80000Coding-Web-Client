9029890f3c70f6a103638aca185c32c35a4285a1
rkskekzzz <wkdlfflxh@naver.com>
2023-09-15 01:20:50 +0900

    ♻️ refactor Chip 컴포넌트 변경사항 적용

--- apps/web/src/app/test/chip/page.tsx
+++ apps/web/src/app/test/chip/page.tsx
@@ -1,30 +1,14 @@
-import { Chip, CommentChip, IconChip, SearchChip } from '@80000coding/ui'
+import { Chip, IconChip } from '@80000coding/ui'
 
 // import { icons } from '@80000coding/web-icons'
 
 export default function Page() {
   return (
     <div className='flex h-screen w-screen flex-col items-start gap-2 bg-[#000] p-20'>
-      <Chip text='인증' size='lg' variant='primary' disabled />
-      <Chip text='인증' size='lg' variant='primary' />
-      <Chip text='대기' size='lg' variant='outline' disabled />
-      <Chip text='완료' size='lg' variant='outline' />
-
-      <Chip text='답글 달기' size='sm' variant='plain' />
-      <Chip text='설정' size='sm' variant='plain' />
-      <Chip text='#태그' size='xs' variant='flat' />
-      <Chip text='+3' size='xs' variant='flat' />
-
-      <IconChip />
-      <IconChip erasable />
-      <IconChip categoryIconNames='TypeScript' />
-      <IconChip erasable categoryIconNames='TypeScript' />
-      <IconChip erasable categoryIconNames='React' variant='primary' />
-
-      <CommentChip />
-      <CommentChip type='cancel' />
-
-      <SearchChip text='최근검색내역' />
+      <Chip text='#인증' />
+      <IconChip categoryIconNames='Swift-rounded' />
+      <IconChip categoryIconNames='TypeScript-rounded' />
+      <IconChip categoryIconNames='AppleDeveloperAcademy-rounded' size='md' />
     </div>
   )
 }
--- packages/ui/src/chip/Chip.tsx
+++ packages/ui/src/chip/Chip.tsx
@@ -1,46 +1,14 @@
 import cn from 'classnames'
 
-type ChipSize = 'xs' | 'sm' | 'md' | 'lg' | 'none'
-type ChipVariant = 'primary' | 'outline' | 'plain' | 'flat' | 'none'
-
 type Props = {
   text?: string
-  size?: ChipSize
-  variant?: ChipVariant
-  disabled?: boolean
-} & React.HTMLAttributes<HTMLButtonElement>
+} & Omit<React.HTMLAttributes<HTMLSpanElement>, 'children'>
 
-const Chip = ({ text, children, className, size = 'none', variant = 'none', disabled = false, ...rest }: Props) => {
-  const sizeToClass = {
-    xs: 'caption-3 py-[5px] px-[10px]',
-    sm: 'note-3 py-[8px] px-[15px]',
-    md: 'body-1 px-[22px] py-[9px]',
-    lg: 'body-1 py-[13px] px-[22px]',
-    none: '',
-  }
-  const variantToClass = {
-    primary: 'bg-green text-white disabled:bg-gray-300',
-    outline:
-      'text-green shadow-[inset_0_0_0_1px] shadow-green bg-white disabled:text-gray-300 disabled:shadow-[inset_0_0_0_1px] disabled:shadow-gray-300',
-    plain: 'bg-white text-gray-600 shadow-[inset_0_0_0_1px] shadow-gray-300',
-    flat: 'bg-gray-200 text-gray-600',
-    none: '',
-  }
-
+const Chip = ({ text, className, ...rest }: Props) => {
   return (
-    <button
-      className={cn(
-        'rounded-full',
-        sizeToClass[size],
-        variantToClass[variant],
-        rest.onClick ? 'cursor-pointer' : 'cursor-default',
-        className,
-      )}
-      disabled={disabled}
-      {...rest}
-    >
-      {text || children}
-    </button>
+    <span className={cn('caption-3 rounded-full bg-gray-200 px-[10px] py-[5px] text-gray-600', className)} {...rest}>
+      {text}
+    </span>
   )
 }
 
