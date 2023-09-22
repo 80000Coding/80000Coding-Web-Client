29d933b74e885639ac0060de99e5a28ed18b4c9a
sehyeon An <sehyeon014@gmail.com>
2023-09-22 23:59:16 +0900

    ✨ Input Box Component
    Co-authored-by: 신수형 suhshin <sushin.bot@gmail.com>

--- apps/web/src/app/test/input/page.tsx
+++ apps/web/src/app/test/input/page.tsx
@@ -1,10 +1,24 @@
 'use client'
 import { Input } from '@80000coding/ui'
+import { useState } from 'react'
 
 export default function Page() {
+  const [value, setValue] = useState('')
+
   return (
-    <div className='flex h-screen w-screen flex-col items-start gap-2 bg-[#000] p-20'>
-      <Input />
+    <div className='flex h-screen w-screen flex-col items-start gap-2 p-10'>
+      <Input value={value} setValue={setValue} />
+      <Input value={value} setValue={setValue} label={'조직 이름'} description={'조직 이름을 입력해 주세요'} />
+      <Input value={value} setValue={setValue} isInvalid={value === '가나다라'} description='' displayLength={false}></Input>
+      <Input
+        value={value}
+        setValue={setValue}
+        isInvalid={false}
+        description=''
+        displayLength={false}
+        isCorrect={value === '가나다라'}
+      ></Input>
+      {/* <InputSearch></InputSearch> */}
     </div>
   )
 }
--- packages/ui/src/input/index.tsx
+++ packages/ui/src/input/index.tsx
@@ -1,30 +1,89 @@
+import { StaticConfirmIcon } from '@80000coding/web-icons'
 import { Input as Input$1 } from '@nextui-org/react'
+import { Dispatch, SetStateAction, useState } from 'react'
 
-export type InputProps = Omit<React.ComponentProps<typeof Input$1>, 'classNames'>
+export type InputProps = {
+  value: string
+  setValue: Dispatch<SetStateAction<string>>
+  displayLength?: boolean
+  isDescription?: boolean
+  isCorrect?: boolean
+} & Omit<React.ComponentProps<typeof Input$1>, 'classNames'>
 
-export function Input() {
+export function Input({
+  value,
+  setValue,
+  isInvalid = false,
+  placeholder = 'placeholder',
+  errorMessage: errorMessage$1 = 'Please enter a valid value',
+  displayLength = true,
+  description: description$1 = 'Please enter a value',
+  isDescription = true,
+  isCorrect = false,
+  label,
+  ...rest
+}: InputProps) {
+  const [isFocus, setIsFocus] = useState(false)
+  const description = displayLength ? (
+    <>
+      <span className='absolute top-[12px]'>{description$1}</span>
+      <span className='absolute right-[12px] top-[12px]'>{value.length + '/20'}</span>
+    </>
+  ) : (
+    description$1
+  )
+
+  const errorMessage = displayLength ? (
+    <>
+      <span className='absolute top-[12px]'>{errorMessage$1}</span>
+      <span className='absolute right-[12px] top-[12px] text-gray-400'>{value.length + '/20'}</span>
+    </>
+  ) : (
+    errorMessage$1
+  )
+
   return (
     <Input$1
-      type='email'
-      label='Email'
-      radius='full'
-      classNames={{
-        label: 'text-black/50 dark:text-white/90',
-        input: ['bg-transparent', 'text-black/90 dark:text-white/90', 'placeholder:text-default-700/50 dark:placeholder:text-white/60'],
-        innerWrapper: 'bg-transparent',
-        inputWrapper: [
-          'shadow-xl',
-          'bg-default-200/50',
-          'dark:bg-default/60',
-          'backdrop-blur-xl',
-          'backdrop-saturate-200',
-          'hover:bg-default-200/70',
-          'focus-within:!bg-default-200/50',
-          'dark:hover:bg-default/70',
-          'dark:focus-within:!bg-default/60',
-          '!cursor-text',
-        ],
+      /* strings */
+      value={value}
+      label={label}
+      placeholder={placeholder}
+      description={isDescription && description}
+      errorMessage={isInvalid && errorMessage}
+      /* status */
+      isInvalid={isInvalid}
+      isClearable={isFocus || !isCorrect}
+      /* actions */
+      onValueChange={setValue}
+      onFocusChange={(e) => {
+        setIsFocus(e)
       }}
+      /* styles */
+      type='text'
+      radius='full'
+      labelPlacement='outside'
+      endContent={isCorrect && !isFocus && <StaticConfirmIcon className='pointer-events-none absolute right-[13px]' />}
+      classNames={{
+        label: ['mx-[12px]', 'body-2'],
+        description: ['w-full', 'text-gray-400', 'caption-2', 'left-[12px]'],
+        errorMessage: ['w-full', 'text-red-warning', 'caption-2', 'left-[12px]'],
+        input: ['!bg-white', 'text-black', 'placeholder:text-gray-300', 'body-3', 'h-100'],
+        innerWrapper: [],
+        inputWrapper: [
+          'items-start',
+          '!bg-white',
+          'border',
+          isInvalid ? 'border-red-warning' : isCorrect ? 'border-green' : 'border-gray-300',
+          isInvalid ? 'hover:border-red-warning' : 'hover:border-green',
+          isInvalid ? 'focus-within:border-red-warning' : 'focus-within:border-green',
+          '!cursor-text',
+          'rounded-[20px]',
+          'px-[20px]',
+          'py-[13px]',
+        ],
+        mainWrapper: 'pb-[28px]', // 28px = padding 12px + lineheight 16px
+      }}
+      {...rest}
     />
   )
 }
