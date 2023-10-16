af449a988542465f0d36fef36c0dcb4169a61074
kunlee <khlee@ebrain.kr>
2023-10-17 01:31:20 +0900

    feat: submit container ui

---
+++ apps/web/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useCallback } from 'react'
+
+import TopbarWithXIcon from '@/components/shared/TopbarWithXIcon'
+import SignupForm from '@/components/signup/SignupForm'
+
+export default function SignupPage() {
+  const router = useRouter()
+
+  const handleClick = useCallback(() => {
+    /** TODO: Alert 생기면 교체 */
+    if (confirm('홈으로 이동하시겠습니까?')) router.push('/')
+  }, [router])
+
+  return (
+    <div className='flex h-full w-full flex-col'>
+      <TopbarWithXIcon title='회원가입' onClose={handleClick} />
+      <SignupForm />
+    </div>
+  )
+}
