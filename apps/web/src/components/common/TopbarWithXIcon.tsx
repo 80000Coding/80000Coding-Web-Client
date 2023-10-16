4e7973e462c5994b141411f020973913c234844e
kunlee <khlee@ebrain.kr>
2023-10-16 22:02:00 +0900

    feat: login 페이지

--- apps/web/src/components/common/TopbarWithXIcon.tsx
+++ apps/web/src/components/common/TopbarWithXIcon.tsx
@@ -11,14 +11,16 @@ type Props = {
 
 export default function TopbarWithXIcon({ title, onAction = () => {}, actionLabel, onClose }: Props) {
   return (
-    <div className='relative flex items-center justify-center p-5'>
-      <StaticCloseIcon onClick={onClose} className='mr-auto h-5 w-5' />
-      {title && <div className='body-1 absolute text-gray-700'>{title}</div>}
-      {actionLabel && (
-        <div onClick={onAction} className='body-2 text-gray-700'>
-          {actionLabel}
-        </div>
-      )}
+    <div className='sticky top-0 w-full bg-white'>
+      <div className='relative flex items-center justify-center p-5'>
+        <StaticCloseIcon onClick={onClose} className='mr-auto h-5 w-5' />
+        {title && <div className='body-1 absolute text-gray-700'>{title}</div>}
+        {actionLabel && (
+          <div onClick={onAction} className='body-2 text-gray-700'>
+            {actionLabel}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
