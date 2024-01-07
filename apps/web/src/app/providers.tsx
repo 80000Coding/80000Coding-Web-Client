12a883341d4a8229df00930d4d23d014fb210d72
rkskekzzz <wkdlfflxh@naver.com>
2024-01-07 23:20:38 +0900

    📦️ `web` 앱에 `next-themes` 패키지 추가

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,7 +1,15 @@
 'use client'
 
+import { ThemeProvider } from '@80000coding/next-themes'
 import { NextUIProvider } from '@nextui-org/react'
+import { useEffect, useState } from 'react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return <NextUIProvider>{mounted ? <ThemeProvider attribute='class'>{children}</ThemeProvider> : children}</NextUIProvider>
 }
