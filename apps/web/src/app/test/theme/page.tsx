8b68d169e7e5224ab799ba4a727574bd2db4ea33
rkskekzzz <wkdlfflxh@naver.com>
2024-01-07 23:59:51 +0900

    ✨ theme test page 추가

---
+++ apps/web/src/app/test/theme/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { Button, ThemeDropdown } from '@80000coding/ui'
+
+export default function AlertPage() {
+  return (
+    <>
+      <Button text='GitHub로 계속하귕' className='bg-black' />
+      <ThemeDropdown />
+    </>
+  )
+}
