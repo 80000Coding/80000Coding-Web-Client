9dee0784f2a158f4d47c094557df96b072879729
rkskekzzz <wkdlfflxh@naver.com>
2024-01-07 23:19:50 +0900

    ✨ ThemeDropdown 추가

---
+++ packages/ui/src/dropdown/ThemeDropdown.tsx
@@ -0,0 +1,37 @@
+import { StaticNewIcon } from '@80000coding/web-icons'
+import { useEffect } from 'react'
+
+import Dropdown from './Dropdown'
+import { useTheme } from '../hooks'
+
+const ThemeDropdown = () => {
+  const { setTheme, theme } = useTheme()
+
+  useEffect(() => {
+    console.log(theme)
+  }, [theme])
+
+  return (
+    <Dropdown
+      menus={[
+        {
+          title: 'sadf',
+          icon: StaticNewIcon,
+          onClick: () => setTheme('light'),
+        },
+        {
+          title: '다크 모드',
+          icon: StaticNewIcon,
+          onClick: () => setTheme('dark'),
+        },
+        {
+          title: '시스템 모드',
+          icon: StaticNewIcon,
+          onClick: () => setTheme('system'),
+        },
+      ]}
+    />
+  )
+}
+
+export default ThemeDropdown
