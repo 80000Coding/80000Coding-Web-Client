d7797c1294dd292819a8930b7dc84af2986ccdb3
sehyeon An <sehyeon014@gmail.com>
2024-01-13 15:56:27 +0900

    🧪 theme page test

--- apps/web/src/app/test/theme/page.tsx
+++ apps/web/src/app/test/theme/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import { Button, ThemeDropdown } from '@80000coding/ui'
+import cn from 'classnames'
 
 export default function AlertPage() {
   return (
     <>
-      <Button text='GitHub로 계속하귕' className='bg-black' />
+      <div className={cn('bg-red', 'dark:bg-white dark:text-black')}>hello</div>
+      <Button text='GitHub로 계속하귕' className='bg-black dark:bg-white' />
+      <Button text='primary' variant='primary' className='bg-green' />
+      <Button text='outline' variant='outline' className='bg-red' />
       <ThemeDropdown />
     </>
   )
